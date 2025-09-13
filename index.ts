1935e956923560e0631d65d5b76586bd8fa22b81
Leonardo Freitas <166871181+leozinr01@users.noreply.github.com>
2025-09-13 19:52:10 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'sobre', loadChildren: () => import('./pages/sobre/sobre.module').then(m => m.SobrePageModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
